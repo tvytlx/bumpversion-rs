278d175679c417bcadab78309ee0202191198087
Xiao Tan <tvytlx@gmail.com>
2019-01-24 22:41:51 +0800

    feat: more commands support

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,67 @@
-use clap::App;
+use clap::{App, Arg};
 use ini;
+use std::collections::HashMap;
 use std::collections::HashSet;
 use std::fs;
 use std::iter::FromIterator;
 
-fn next_version(current_version: &str) -> &str {
-    "0.1.1"
+fn next_version(config: &HashMap<String, String>, part: &String) -> String {
+    let mut version = parse_version(config.get("current_version").unwrap().to_string())
+        .take()
+        .unwrap();
+    match part.as_str() {
+        "major" => version.major += 1,
+        "minor" => version.minor += 1,
+        "patch" => {
+            if version.patch != None {
+                version.patch = Some(version.patch.unwrap() + 1);
+            } else {
+                version.patch = Some(1);
+            }
+        }
+        _ => panic!("wrong part"),
+    };
+    if version.patch != None {
+        format!(
+            "{}.{}.{}",
+            version.major,
+            version.minor,
+            version.patch.unwrap()
+        )
+    } else {
+        format!("{}.{}", version.major, version.minor)
+    }
 }
 
-fn bump(config: ini::Ini) {
-    // get the bumpversion section
-    let section = config.section(Some("bumpversion".to_owned())).unwrap();
+struct SemanticVersion {
+    major: i32,
+    minor: i32,
+    patch: Option<i32>,
+}
+
+fn parse_version(version: String) -> Option<Box<SemanticVersion>> {
+    let parts = version.split(".").collect::<Vec<&str>>();
+    let major = parts[0].to_string();
+    let minor = parts[1].to_string();
+    let mut patch = None;
+    if parts.len() == 3 {
+        patch = Some(parts[2].to_string().parse::<i32>().unwrap());
+    }
+    Some(Box::new(SemanticVersion {
+        major: major.parse::<i32>().unwrap(),
+        minor: minor.parse::<i32>().unwrap(),
+        patch: patch,
+    }))
+}
+
+fn bump(ini_config: ini::Ini, part: &String) {
+    // get the bumpversion config
+    let config = ini_config.section(Some("bumpversion".to_owned())).unwrap();
 
     // get config
-    let current_version = section.get("current_version").unwrap();
+    let current_version = config.get("current_version").unwrap();
     let files = HashSet::<&str>::from_iter(
-        section
+        config
             .get("files")
             .unwrap()
             .split(",")
@@ -31,7 +77,7 @@ fn bump(config: ini::Ini) {
                 .lines()
                 .map(|line| {
                     if line.contains(current_version) {
-                        line.replace(current_version, next_version(current_version))
+                        line.replace(current_version, next_version(config, &part).as_str())
                     } else {
                         line.to_string()
                     }
@@ -44,22 +90,55 @@ fn bump(config: ini::Ini) {
 }
 
 fn main() {
-    App::new("bumpversion-rs")
-        .version("1.0")
-        .about("bumpversion rust version!")
-        .author("Xiao Tan")
+    let matches = App::new("bumpversion-rs")
+        .version("0.1.0")
+        .about("bump your project's version !")
+        .arg(
+            Arg::with_name("config_file")
+                .short("c")
+                .long("config")
+                .value_name("FILE")
+                .help("config file")
+                .takes_value(true),
+        )
+        .arg(
+            Arg::with_name("part")
+                .value_name("PART")
+                .help("the part of the version to increase, e.g. minor")
+                .required(true)
+                .index(1),
+        )
         .get_matches();
+    let config_file = matches.value_of("config_file").unwrap_or(".setup.cfg");
+    let part = matches.value_of("part").unwrap();
+
+    let config = ini::Ini::load_from_file(config_file).unwrap();
+    bump(config, &part.to_string());
 }
 
 #[cfg(test)]
 mod test {
     use super::bump;
     use super::ini;
+    use super::parse_version;
 
     #[test]
-    fn parse() {
+    fn test_parse() {
         let config_file = ".setup.cfg";
         let config = ini::Ini::load_from_file(config_file).unwrap();
-        bump(config);
+        bump(config, &"major".to_string());
+    }
+
+    #[test]
+    fn test_parse_version() {
+        let mut version = parse_version("1.2.3".to_string()).unwrap();
+        assert_eq!(version.major, 1);
+        assert_eq!(version.minor, 2);
+        assert_eq!(version.patch.unwrap(), 3);
+
+        version = parse_version("1.2".to_string()).unwrap();
+        assert_eq!(version.major, 1);
+        assert_eq!(version.minor, 2);
+        assert_eq!(version.patch, None);
     }
 }
